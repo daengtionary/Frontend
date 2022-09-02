cab9144ad1afeaf934006f6fa499b71ecedc947a
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-09-02 22:16:37 +0900

    feat: Detail 페이지 모달 형태 수정, 카카오지도API 위도 경도값으로 마커를 찍는 방식에서 주소로 마커를 찍는 방식으로 변경

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,27 +1,67 @@
-/* global kakao */
 import React, { useEffect } from "react";
 import { BsXLg } from "react-icons/bs";
 import { MapContainer, MapWrap, ModalBackground, CloseBtm, MapTitle, MapAddress } from "./Map.styled";
 const { kakao } = window;
 
 const Map = ({ modalHandler }) => {
-  const lat = 37.483582263644934;
-  const lon = 126.99202327334409;
+  const lat = 37.5023088;
+  const lon = 127.044437;
 
   const businessName = "여기에 업체명";
-  const businessAddress = "여기에 주소";
+  const businessAddress = "서울특별시 서초구 방배로 19길 67";
 
   useEffect(() => {
     let container = document.getElementById("map");
 
     let options = {
       center: new window.kakao.maps.LatLng(lat, lon),
-      level: 4,
+      level: 3,
     };
 
     let map = new window.kakao.maps.Map(container, options);
 
-    console.log("loading kakaomap");
+    // 주소-좌표 변환 객체를 생성
+    let geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색
+    geocoder.addressSearch(businessAddress, function (result, status) {
+      console.log(result);
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+          map: map,
+          position: coords,
+        });
+
+        // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성
+        let iwContent = `
+        <div style="padding:10px;">
+          <div>${businessName}</div>
+          <div>
+            <a href="https://map.kakao.com/link/map/${businessName},${result[0].y},${result[0].x}" style="color:blue; text-decoration: none;" target="_blank">자세히 보기</a>
+          </div>
+        </div>`;
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+          content: iwContent,
+          removable: true,
+        });
+        infowindow.open(map, marker);
+
+        // 마커에 클릭이벤트를 등록
+        kakao.maps.event.addListener(marker, "click", () => {
+          // 마커 위에 인포윈도우를 표시
+          infowindow.open(map, marker);
+        });
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+      }
+    });
 
     // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
     let mapTypeControl = new kakao.maps.MapTypeControl();
@@ -33,49 +73,15 @@ const Map = ({ modalHandler }) => {
     // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성
     let zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-    // 마커가 표시될 위치
-    let markerPosition = new kakao.maps.LatLng(lat, lon);
-
-    // 마커 생성
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
-      clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-
-    // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-    let iwContent = `
-    <div style="padding:10px;">
-      <div>${businessName}</div>
-      <div>${businessAddress}</div>
-      <div>
-        <a href="https://map.kakao.com/link/map/${businessName},${lat},${lon}" style="color:blue; text-decoration: none;" target="_blank">자세히 보기</a>
-      </div>  
-    </div>`,
-      // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-    // 인포윈도우를 생성합니다
-    let infowindow = new kakao.maps.InfoWindow({
-      content: iwContent,
-      removable: iwRemoveable,
-    });
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, "click", () => {
-      // 마커 위에 인포윈도우를 표시합니다
-      infowindow.open(map, marker);
-    });
   }, []);
 
   return (
-    <ModalBackground>
-      <MapContainer>
+    <ModalBackground onClick={modalHandler}>
+      <MapContainer onClick={(e) => e.stopPropagation()}>
         <CloseBtm>
-          <button onClick={modalHandler}><BsXLg/></button>
+          <button onClick={modalHandler}>
+            <BsXLg />
+          </button>
         </CloseBtm>
         <MapWrap id="map"></MapWrap>
         <MapAddress>여기에 업체 주소</MapAddress>
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react'
+import { SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import Footer from '../../components/footer/Footer'
 import Map from '../../components/map/Map'
+import { GrMapLocation } from "react-icons/gr";
+import { MainBanner ,StyledSwiper, DetailContainer, BusinessTitle, MapAddress, BusinessDescription, ReviewWrap } from './Detail.styled';
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+SwiperCore.use([Pagination, Autoplay, Navigation]);
 
 const Detail = () =>{
 
@@ -10,18 +20,43 @@ const Detail = () =>{
   }
 
   return(
-    <div>
-      <div>여기에 업체 등록 사진</div>      
-      <div>여기에 업체명</div>      
-      <div>
-        <span onClick={modalHandler}>지도</span>
-        <span>주소</span>
-      </div>      
-      <div>여기에 상세 설명</div>      
-      <div>여기에 예약 현황?</div>      
-      <div>후기 글</div>
+    <DetailContainer>
+      <StyledSwiper
+        className="swiper-container"
+        spaceBetween={0}
+        slidesPerView={2}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 9000, disableOnInteraction: false }}
+        loop={true}
+        centeredSlides={true}
+      >
+        <SwiperSlide>
+          <MainBanner background={"#0000ff50"}></MainBanner>
+        </SwiperSlide>
+        <SwiperSlide>
+          <MainBanner background={"#00ff0050"}></MainBanner>
+        </SwiperSlide>
+        <SwiperSlide>
+          <MainBanner></MainBanner>
+        </SwiperSlide>
+      </StyledSwiper>    
+
+      <BusinessTitle><span>여기에 업체명</span></BusinessTitle>      
+      <MapAddress>
+        <span onClick={modalHandler}><GrMapLocation size={24}/></span>
+        <span>서울특별시 어쩌구 저쩌동</span>
+      </MapAddress>      
+      <BusinessDescription>
+        <p>여기에 상세 설명</p>
+      </BusinessDescription>      
+      {/* <div>여기에 예약 현황?</div>       */}
+      <ReviewWrap>후기 글들</ReviewWrap>
+      
       {mapModal && <Map modalHandler={modalHandler}/>}
-    </div>
+
+      <Footer/>
+    </DetailContainer>
   )
 }
 
