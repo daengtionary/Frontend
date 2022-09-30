5ad25a64d39204c3ed9b5ac341b97d1189023dd6
cordplace <msk7316@gmail.com>
2022-09-30 22:07:23 +0900

    refactor: 채팅 api 변경

--- src/shared/api.js
+++ src/shared/api.js
@@ -47,7 +47,7 @@ chatApi.interceptors.request.use(function (config) {
 export const chatApis = {
   // 채팅
   getRoomList: () => chatApi.get("/chat/rooms"),
-  getMessageList: (roomId) => chatApi.get("/chat/message/" + roomId),
+  getMessageList: (roomId) => chatApi.get("/chat/messages/" + roomId),
   addRoom: (memberNo) => chatApi.post("/chat/room/personal", { memberNo }),
   exitRoom: (roomId) => chatApi.get(`chat/room/exit/${roomId}`),
   addMatchingRoom: () => chatApi.post("/chat/room/group"),
